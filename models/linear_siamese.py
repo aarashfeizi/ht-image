940b5ad986fe0df662feed4f2d6ad425f98499f1
Aarash Feizi <aarash95@gmail.com>
2020-08-18 12:30:55 +0430

    bug in concat

--- models/linear_siamese.py
+++ models/linear_siamese.py
@@ -46,7 +46,8 @@ class LiSiamese(nn.Module):
 
         out2 = self.forward_one(x2)
 
-        out_cat = torch.cat((out1, out2), 0)
+
+        out_cat = torch.cat((out1, out2), 1)
 
         # dis = torch.abs(out1 - out2)
 
