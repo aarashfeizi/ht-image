df0a96e535305d63c1060ff5c8f28255c37fb6ae
Aarash Feizi <aarash95@gmail.com>
2021-07-07 10:35:39 -0400

    gen hotels for v4

--- genHotels.py
+++ genHotels.py
@@ -16,6 +16,7 @@ SPLITS = ['train',
 def get_args():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--splits', nargs='+', default=[])
     parser.add_argument('--split_path', type=str)
     parser.add_argument('--dataset_path', type=str)
     parser.add_argument('--target_path', type=str)
@@ -39,9 +40,9 @@ def restructure(dataset_path, target_path, path_csv, prefix):
 def main():
     args = get_args()
 
-    for prefix in SPLITS:
-        print(f'Doing {args.dataset + "_" + prefix}')
-        p = os.path.join(args.split_path, args.dataset + "_" + prefix)
+    for prefix in args.splits:
+        print(f'Doing {prefix}')
+        p = os.path.join(args.split_path, prefix)
         path_csv = pd.read_csv(p + '.csv')
         restructure(args.dataset_path, args.target_path, path_csv, prefix)
 
