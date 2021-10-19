8e886c3abc884ece0df353e0628ac85bbc84060c
Aarash Feizi <aarash95@gmail.com>
2021-10-18 21:27:40 -0400

    add evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,19 @@
+import os
+
 import numpy as np
 import time
 import faiss
+import torchvision.models
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import roc_auc_score
 from tqdm import tqdm
 import argparse
 import pickle
 import torch
+
+import dataset_loaders
+
+dataset_choices = ['cars', 'cub', 'hotels']
 
 # softtriplet loss code
 def evaluate_recall_at_k(X, Y, Kset, gpu=False, k=5, metric='cosine', dist_matrix=None, ):
@@ -140,37 +147,95 @@ def get_faiss_knn(reps, k=1000, gpu=False, metric='cosine'):  # method "cosine" 
 
     return D, I, self_D
 
+def get_features_and_labels(args, model, loader):
+    features = []
+    labels = []
+
+    for idx, batch in enumerate(loader):
+        img, lbl = batch
+        if args.cuda:
+            f = model(img.cuda())
+        else:
+            f = model(img)
+
+        features.append(f.cpu().detach().numpy())
+        labels.append(lbl)
+
+    return features, labels
+
+def load_model_resnet50(save_path, args):
+    if args.cuda:
+        checkpoint = torch.load(save_path)
+    else:
+        checkpoint = torch.load(save_path, map_location=torch.device('cpu'))
+
+    net = torchvision.models.resnet50(False)
+
+    net.load_state_dict(checkpoint)
+
+    return net
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-X', '--X', default='features.pkl')
-    parser.add_argument('-Y', '--Y', default='labels.pkl')
+    parser.add_argument('-cuda', '--cuda', default=False, action='store_true')
+    parser.add_argument('-X', '--X', default=None, type=str)  # 'features.pkl'
+    parser.add_argument('-Y', '--Y', default=None, type=str)  # 'labels.pkl'
+    parser.add_argument('-d', '--dataset', default='hotels', choices=dataset_choices)
+    parser.add_argument('-dr', '--data_root', default='../hotels')
+    parser.add_argument('-chk', '--checkpoint', default=None, help='Path to checkpoint')
     parser.add_argument('--kset', nargs='+', default=[1, 2, 4, 8])
     parser.add_argument('--roc_n', default=0, type=int)
     parser.add_argument('--metric', default='cosine', choices=['cosine', 'euclidean'])
 
     args = parser.parse_args()
+    all_data = []
+    if args.dataset is not None:
+        eval_ldrs = []
+        if args.dataset == 'hotels':
+            for i in range(1, 5):
+                eval_ldrs.append(dataset_loaders.load(
+                name=args.dataset,
+                root=args.data_root,
+                transform=dataset_loaders.utils.make_transform(
+                    is_train=False,
+                    ),
+                valset=i))
+        else:
+            eval_ldrs = [dataset_loaders.load(
+                name=args.dataset,
+                root=args.data_root,
+                transform=dataset_loaders.utils.make_transform(
+                    is_train=False,
+                    ))]
 
-    print(f'Calc Recall at {args.kset}')
-    with open(args.X, 'rb') as f:
-        features = pickle.load(f)
-        if torch.is_tensor(features):
-            features = features.cpu().numpy()
+        net = load_model_resnet50(args.checkpoint, args)
+        for ldr in eval_ldrs:
+            features, labels = get_features_and_labels(args, net, ldr)
+            all_data.append((features, labels))
 
-    with open(args.Y, 'rb') as f:
-        labels = pickle.load(f)
-        if torch.is_tensor(labels):
-            labels = labels.cpu().numpy()
+    else: # X and Y should be provided
+        with open(args.X, 'rb') as f:
+            features = pickle.load(f)
+            if torch.is_tensor(features):
+                features = features.cpu().numpy()
 
-    rec = evaluate_recall_at_k(features, labels, Kset=args.kset, metric=args.metric)
-    print(args.kset)
-    print(rec)
+        with open(args.Y, 'rb') as f:
+            labels = pickle.load(f)
+            if torch.is_tensor(labels):
+                labels = labels.cpu().numpy()
+        all_data.append((features, labels))
 
-    print('*' * 10)
-    print('Calc AUC_ROC')
-    auc = evaluate_roc(features, labels, n=args.roc_n)
-    print('AUC_ROC:', auc)
+    for idx, (features, labels) in enumerate(all_data, 1):
+        print(f'{idx}: Calc Recall at {args.kset}')
+        rec = evaluate_recall_at_k(features, labels, Kset=args.kset, metric=args.metric)
+        print(args.kset)
+        print(rec)
+
+        print('*' * 10)
+        print(f'{idx}: Calc AUC_ROC')
+        auc = evaluate_roc(features, labels, n=args.roc_n)
+        print(f'{idx}: AUC_ROC:', auc)
 
 if __name__ == '__main__':
     main()
